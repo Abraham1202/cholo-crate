1f161cc749fee23f58d62b3021b85b6eb6132e8a
Abraham1202 <137967472+Abraham1202@users.noreply.github.com>
2024-10-21 22:18:58 -0500

    Update aboutpage.js

--- src/pages/aboutpage.js
+++ src/pages/aboutpage.js
@@ -6,9 +6,9 @@ function AboutPage() {
       <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">Sobre Cholo Crate</h2>
       <div className="bg-white shadow-lg rounded-lg p-6">
         <p className="text-gray-700 mb-4">
-          Cholo Crate es tu destino número uno para productos auténticos de la cultura chola. 
+          Cholo Crate es tu destino número uno para productos auténticos de la cultura panameña. 
           Fundada en 2024, nuestra misión es proporcionar productos de alta calidad que celebran 
-          y honran la rica herencia de la cultura chola.
+          y honran la rica herencia de la cultura panameña.
         </p>
         <p className="text-gray-700">
           Nos enorgullecemos de ofrecer una amplia gama de productos, desde ropa y accesorios 
